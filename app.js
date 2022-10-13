80185c4302e86e2cadbdd80bcb8ff44d6a0ddc24
Vianney <thescraft69@gmail.com>
2022-10-13 13:11:50 +0200

    Adding a typeWrting Animation at the start of the website
    Added the gasp library to the site

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const navmenu = document.querySelector(".navmenu");
+const startingAnim = document.querySelectorAll(".startingAnim");
+let typeWriterIteration = 0 ;
+const typeWriterElem = document.querySelector(".hello");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -8,4 +11,18 @@ hamburger.addEventListener("click", () => {
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navmenu.classList.remove("active")
-}) )
+}) );
+gsap.set(".startingAnim", {opacity: 0});
+
+function typeWriterAnimFinished (){
+    gsap.set(".startingAnim", {opacity: 1});
+    var tl = gsap.timeline();
+    tl.from("header", {y: -100, opacity:0 ,duration: 1.3, ease: 'power1'} );
+    tl.from(".second-child, .bonjour", {x: -100, opacity:0 , duration: 1.3,ease:'power1'}, "<");
+    tl.from(".cool, .tete", {x: 100,opacity: 0, duration: 1.3 , ease:'power1'},"<");
+   
+};
+
+Promise.all(typeWriterElem.getAnimations().map((animation) => animation.finished)).then(
+    () => typeWriterAnimFinished()
+);
