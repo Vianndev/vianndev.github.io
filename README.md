b529a9f35469d994758e364e5307923186606989
Vianney <thescraft69@gmail.com>
2022-10-14 12:31:52 +0200

    Adding a README

---
+++ README.md
@@ -0,0 +1,18 @@
+# Portfolio de Vianney
+_(juste en dessous des badges sympatiques à placer)_
+
+[![forthebadge](http://forthebadge.com/images/badges/built-with-love.svg)](http://forthebadge.com)
+
+Voici mon tout premier reel projet de developpement web , j'ai donc realisé mon cv de maniere responsive , en utilisant html , css et js.
+J'ai utilisé la libraire Gasp de GreenSocks pour mettre en place quelques animations.
+J'ai appris à utiliser les media queries , flexbox et un peu plus javascript grace a ce projet.
+
+## Fabriqué avec
+
+Entrez les programmes/logiciels/ressources que vous avez utilisé pour développer votre projet
+
+_exemples :_
+* [JS](https://developer.mozilla.org/fr/docs/Web/JavaScript) - JavaScript
+* [HTML](https://developer.mozilla.org/fr/docs/Web/HTML) - HTML
+* [CSS](https://developer.mozilla.org/fr/docs/Web/CSS) - CSS 
+* [Visual Studio](https://code.visualstudio.com/) - Editeur de textes
--- app.js
+++ app.js
@@ -14,20 +14,22 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 }) );
 gsap.set(".startingAnim", {opacity: 0});
 
-
 function typeWriterAnimFinished (){
     gsap.set(".startingAnim", {opacity: 1});
     var tl = gsap.timeline();
     tl.from("header", {y: -100, opacity:0 ,duration: 1.3, ease: 'power1'} );
     tl.from(".second-child, .bonjour", {x: -100, opacity:0 , duration: 1.3,ease:'power1'}, "<");
     tl.from(".cool, .tete", {x: 100,opacity: 0, duration: 1.3 , ease:'power1'},"<");
-   
+    setTimeout(scrollAnim, 1000 );
 };
 
 Promise.all(typeWriterElem.getAnimations().map((animation) => animation.finished)).then(
     () => typeWriterAnimFinished()
 );
-gsap.from(".xp-title", { scrollTrigger: ".xp-title", opacity: 0, y: 100, duration: 3});
-gsap.from(".fade-right", { scrollTrigger: ".fade-right", opacity: 0, x: 100, duration: 1});
-gsap.from(".fade-left", { scrollTrigger: ".fade-left", opacity: 0, x: -100, duration: 1});
+function scrollAnim () {
+ gsap.from(".xp-title", { scrollTrigger: ".xp-title", opacity: 0, y: 100, duration: 3});
+gsap.from(".fade-right", { scrollTrigger: ".fade-right", opacity: 0, x: 100, duration: 1},"<");
+gsap.from(".fade-left", { scrollTrigger: ".fade-left", opacity: 0, x: -100, duration: 1},"<");;
+}
+
 
